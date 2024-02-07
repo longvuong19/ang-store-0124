17ae5ccc0dbb1ee8713ecdff56481c1c1bd8fe68
longvuong19 <longvuong1920@gmail.com>
2024-02-07 17:33:36 +0700

    add cart logic

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, filter } from "rxjs";
 import { Cart, CartItem } from "../models/cart.model";
 import { MatSnackBar } from "@angular/material/snack-bar";
 
@@ -23,6 +23,51 @@ export class CartService {
 
     this.cart.next({ items: items });
     this._snackBar.open("1 item added to cart", "OK", { duration: 3000 });
-    console.log(this.cart.value);
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
+
+  clearCart(): void {
+    this.cart.next({ items: [] });
+    this._snackBar.open("Cart is cleared.", "OK", { duration: 3000 });
+  }
+
+  removeQuantity(item: CartItem): void {
+    let itemForRemoval: CartItem | undefined;
+
+    let filteredItem = this.cart.value.items.map((_item) => {
+      if (_item.id === item.id) {
+        _item.quantity--;
+        if (_item.quantity === 0) {
+          itemForRemoval = _item;
+        }
+      }
+
+      return _item;
+    });
+
+    if (itemForRemoval) {
+      (filteredItem = this.removeFromCart(itemForRemoval)), false;
+    }
+
+    this.cart.next({ items: filteredItem });
+    this._snackBar.open("1 item has been removed.", "OK", { duration: 3000 });
+  }
+
+  removeFromCart(item: CartItem, update = true): Array<CartItem> {
+    const filteredItem = this.cart.value.items.filter(
+      (_item) => _item.id !== item.id
+    );
+
+    if (update) {
+      this.cart.next({ items: filteredItem });
+      this._snackBar.open("1 item has been removed.", "OK", { duration: 3000 });
+    }
+
+    return filteredItem;
   }
 }
